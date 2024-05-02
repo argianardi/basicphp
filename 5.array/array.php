901eeebade33526357469c1c215f7a60acdda9f3
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-02 10:11:40 +0700

    Feat: Add array

---
+++ 5.array/array.php
@@ -0,0 +1,17 @@
+<?php 
+$bulan = ['januari', 'februari', 'maret'];
+
+// Menampilkan keseluruh value array, gunaka var_dump() / print_r()
+// var_dump($bulan); // array(3) { [0]=> string(7) "januari" [1]=> string(8) "februari" [2]=> string(5) "maret" }
+// print_r($bulan); // Array ( [0] => januari [1] => februari [2] => maret ) 
+
+// Menampilkan satu value array
+// echo $bulan[1]; // februari
+
+// Menambahkan elemen baru pada array
+// print_r($bulan);
+// $bulan[] = "april";
+// echo "<br>";
+// print_r($bulan);
+
+?>
